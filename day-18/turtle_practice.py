b84810b814f1a57197cef2207339864cad9707c7
pethe <pethersonreis@gmail.com>
2024-02-20 15:02:59 -0300

    feat: draw_dashes, draw_shapes and random_walk functions

--- day-18/turtle_practice.py
+++ day-18/turtle_practice.py
@@ -11,6 +11,14 @@ def draw_dashes():
         pendown()
 
 
+# Random Colors
+def random_colors():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
+
+
 # draw shapes
 def draw_shapes():
     shapes_colors = {"CornFlowerBlue": 3, "DarkOrchid": 4, "IndianRed": 5, "DeepSkyBlue": 6, "LightSeaGreen": 7,
@@ -30,27 +38,41 @@ def draw_shapes():
 def random_walk():
     image_size = 333
     # Setup Turtle
-    pensize(10)  # Set size
-    speed(10)  # Set the speed to maximum
+    pensize(15)  # Set size
 
-    colors = ["CornFlowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen",
-              "wheat", "SlateGray", "SeaGreen"]
+
+    # colors = ["CornFlowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen",
+    #     #           "wheat", "SlateGray", "SeaGreen"]
 
     directions = [0, 90, 180, 270]
 
     # Draw a colorful pattern
     for _ in range(image_size):
         direction = random.choice(directions)
-        color(random.choice(colors))
-        forward(20)
+        color((random_colors()))
+        forward(30)
         setheading(direction)
+
+
+# Spirograph
+def spirograph(size_of_gap):
+    for _ in range(360 // size_of_gap):
+        color(random_colors())
+        circle(100)
+        current_heading = heading()
+        setheading(current_heading + size_of_gap)
 
 
 def __main__():
     screen = Screen()
 
+    speed("fastest")  # Set the speed to maximum
+
+    # Change colormode
+    screen.colormode(255)
+
     # Call the functions bellow
-    random_walk()
+    spirograph(5)
 
     # Hide the turtle and display the result
     hideturtle()
