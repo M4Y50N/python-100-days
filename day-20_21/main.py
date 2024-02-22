ef10910c0a78884bcba474bc08b667f04c0befcf
pethe <pethersonreis@gmail.com>
2024-02-22 20:53:55 -0300

    fix: cleaning the code

--- day-20_21/main.py
+++ day-20_21/main.py
@@ -2,30 +2,29 @@ from turtle import Screen
 from Snake import Snake
 import time
 
+# Setup screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
+# Initialize snake
 snake = Snake()
 
+# Listening to the keys to move the snake
 screen.listen()
 screen.onkey(snake.move_left, "Left")
 screen.onkey(snake.move_right, "Right")
 screen.onkey(snake.move_up, "Up")
 screen.onkey(snake.move_down, "Down")
 
-
+# Run game
 game_is_on = True
 while game_is_on:
     # Prevent/Allow screen to render
     screen.update()
     time.sleep(0.1)
-
-    if snake.size < 10:
-        snake.size += 1
-        snake.update_size()
 
     snake.move()
 
