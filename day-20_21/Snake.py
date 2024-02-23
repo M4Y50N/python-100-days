1bdab8c37fc06d91d7de6e8a76753031d3b2f840
pethe <pethersonreis@gmail.com>
2024-02-23 20:43:04 -0300

    feat: snake game completed

--- day-20_21/Snake.py
+++ day-20_21/Snake.py
@@ -1,25 +1,37 @@
 from turtle import Turtle
 
+COLOR = "white"
+
+STEP = 20
+
+RIGHT = 0
+LEFT = 180
+UP = 90
+DOWN = 270
+
 
 class Snake(Turtle):
     def __init__(self):
         super().__init__()
+        self.hideturtle()
         self.size = 3
         self.body = []
         self.create_snake()
         self.head = self.body[0]
 
     def create_snake(self):
-        for _ in range(self.size):
-            snake = Turtle("square")
-            snake.penup()
-            snake.color("white")
-            self.body.append(snake)
+        for i in range(self.size):
+            self.add_segment((i * -20, 0))
 
-        [part.goto(i * -20, 0) for i, part in enumerate(self.body)]
+    def add_segment(self, pos):
+        new_segment = Turtle("square")
+        new_segment.color(COLOR)
+        new_segment.penup()
+        new_segment.goto(pos)
+        self.body.append(new_segment)
 
     def update_size(self):
-        ...
+        self.add_segment(self.body[-1].position())
 
     def move(self):
         for seg_num in range(len(self.body) - 1, 0, -1):
@@ -28,20 +40,20 @@ class Snake(Turtle):
             self.body[seg_num].goto(x=next_x, y=next_y)
 
         # Snake's head
-        self.head.forward(20)
+        self.head.forward(STEP)
 
     def move_up(self):
-        if self.head.heading() != 270:
-            self.head.seth(90)
+        if self.head.heading() != DOWN:
+            self.head.seth(UP)
 
     def move_down(self):
-        if self.head.heading() != 90:
-            self.head.seth(270)
+        if self.head.heading() != UP:
+            self.head.seth(DOWN)
 
     def move_left(self):
-        if self.head.heading() != 0:
-            self.head.seth(180)
+        if self.head.heading() != RIGHT:
+            self.head.seth(LEFT)
 
     def move_right(self):
-        if self.head.heading() != 180:
-            self.head.seth(0)
+        if self.head.heading() != LEFT:
+            self.head.seth(RIGHT)
--- day-20_21/main.py
+++ day-20_21/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from Snake import Snake
+from Food import Food
+from Score import Score
 import time
 
 # Setup screen
@@ -9,8 +11,11 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-# Initialize snake
+# Initialize snake and food
+food = Food()
 snake = Snake()
+score = Score()
+
 
 # Listening to the keys to move the snake
 screen.listen()
@@ -25,7 +30,24 @@ while game_is_on:
     # Prevent/Allow screen to render
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        score.increment()
+        snake.update_size()
+        food.refresh()
+
+    # Detect collision with wall
+    if (snake.head.xcor() > 290 or snake.head.xcor() < -290
+            or snake.head.ycor() > 290 or snake.head.ycor() < -290):
+        game_is_on = False
+        score.game_over()
+
+    # Detect collision with til
+    for segment in snake.body[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
 
 screen.exitonclick()
