3a8414f1c597a471559d42eab4a81c689d78d8b2
pethe <pethersonreis@gmail.com>
2024-03-01 00:24:47 -0300

    feat: add missing states csv file

--- day-25/main.py
+++ day-25/main.py
@@ -12,19 +12,25 @@ screen.addshape(image)
 turtle.shape(image)
 
 data_states = pandas.read_csv("./50_states.csv")
-states = data_states["state"].to_list()
+all_states = data_states["state"].to_list()
 
-screen.tracer(0)
 
 guessed_states = []
 while len(guessed_states) < 50:
-    time.sleep(.5)
-    screen.update()
-    answer_state = screen.textinput(f"{len(guessed_states)}/50 Guess the State", "What's another state's name?").capitalize()
+    answer_state = screen.textinput(f"{len(guessed_states)}/50 Guess the State",
+                                    "What's another state's name?").title()
 
-    if answer_state in states and answer_state not in guessed_states:
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        pandas.DataFrame(missing_states).to_csv("missing_states.csv")
+        break
+
+    if answer_state in all_states and answer_state not in guessed_states:
         guessed_states.append(answer_state)
         state = data_states[data_states["state"] == answer_state]
-        State(answer_state, (state.x.to_list()[0], state.y.to_list()[0]))
+        State(answer_state, (int(state.x), int(state.y)))
 
-screen.mainloop()
+# screen.mainloop()
