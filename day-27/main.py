c419f672faa995b460da49a46944645e98ee1c49
pethe <pethersonreis@gmail.com>
2024-03-02 16:17:52 -0300

    fix: fix griding layout

--- day-27/main.py
+++ day-27/main.py
@@ -13,23 +13,23 @@ window.config(padx=20, pady=20)
 
 # Label
 miles = Label(text="Miles", font=FONT)
-miles.grid(column=3, row=1)
+miles.grid(column=2, row=0)
 
 # Entry
 text_input = Entry()
 text_input.config(width=10)
-text_input.grid(column=2, row=1)
+text_input.grid(column=1, row=0)
 
 # Button
 calc_button = Button(text="Calculate", command=convert_to_km)
-calc_button.grid(column=2, row=3)
+calc_button.grid(column=1, row=2)
 
 # KM Label
 is_equal = Label(text="is equal to ", font=FONT)
-is_equal.grid(column=1, row=2)
+is_equal.grid(column=0, row=1)
 res_text = Label(font=FONT)
-res_text.grid(column=2, row=2)
+res_text.grid(column=1, row=1)
 km = Label(text="Km", font=FONT)
-km.grid(column=3, row=2)
+km.grid(column=2, row=1)
 
 window.mainloop()
