5457151f2396dd3945fbf653766108d73ee5be26
pethe <pethersonreis@gmail.com>
2024-03-07 20:16:23 -0300

    feat: generate random words

--- day-31/main.py
+++ day-31/main.py
@@ -1,25 +1,41 @@
 from tkinter import *
+import pandas
+import random
 
 FONT_NAME = "Ariel"
 BACKGROUND_COLOR = "#B1DDC6"
 
+
+# Random Words
+def random_word():
+    data = pandas.read_csv("./data/french_words.csv")
+
+    canvas.itemconfig(title, text="French")
+    canvas.itemconfig(word, text=random.choice(data["French"]))
+
+
+# UI
 window = Tk()
 window.title("")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(width=1000, height=670)
-canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.grid(row=0, column=0)
+canvas = Canvas(width=800, height=544, highlightthickness=0)
+canvas.config(bg=BACKGROUND_COLOR)
+canvas.grid(row=0, column=0, columnspan=2)
 
 card_front = PhotoImage(file="./images/card_front.png")
-canvas.create_image(500, 270, image=card_front)
+canvas.create_image(400, 270, image=card_front)
 
-title = canvas.create_text(500, 180, text="Title", font=(FONT_NAME, 24, "italic"))
-word = canvas.create_text(500, 265, text="Word", font=(FONT_NAME, 36, "bold"))
+title = canvas.create_text(400, 180, text="Title", font=(FONT_NAME, 24, "italic"))
+word = canvas.create_text(400, 265, text="Word", font=(FONT_NAME, 36, "bold"))
 
 right = PhotoImage(file="./images/right.png")
 wrong = PhotoImage(file="./images/wrong.png")
-canvas.create_image(700, 600, image=right)
-canvas.create_image(300, 600, image=wrong)
+right_button = Button(image=right, highlightthickness=0, cursor="hand2", command=random_word)
+right_button.grid(row=1, column=1)
+wrong_button = Button(image=wrong, highlightthickness=0, cursor="hand2", command=random_word)
+wrong_button.grid(row=1, column=0)
+
+random_word()
 
 window.mainloop()
