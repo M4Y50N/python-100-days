0df59bea9bec1b0c56c58dc3fd9c4e0878a82a0d
pethe <pethersonreis@gmail.com>
2024-03-10 23:48:12 -0300

    feat: retrieving today's birthday and customize a letter

--- day-32/main.py
+++ day-32/main.py
@@ -1,22 +1,19 @@
 import datetime as dt
-import smtplib
 import random
+import pandas
 
-now = dt.datetime.now()
-my_email = "pethersonreis@gmail.com"
-password = "mpcf zyug mygv zefj"
+today = dt.datetime.now()
+today_tuple = (today.month, today.day)
 
-if now.weekday() == 6:
-    with open("./quotes.txt", mode="r", encoding="utf-8") as file:
-        data = file.read()
-        quotes = data.split('\n')
-        day_quote = random.choice(quotes)
-        print(day_quote)
+data = pandas.read_csv("./birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
-    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-        connection.starttls()
-        connection.login(user=my_email, password=password)
-        connection.sendmail(
-            from_addr=my_email,
-            to_addrs="mayson.reiz@gmail.com",
-            msg=f"Subject:Monday Motivation\n\n{day_quote}".encode("utf-8"))
+# Check today's birthday
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents.replace("[NAME]", birthday_person["name"].capitalize())
+
+
---
+++ day-32/monday_quotes_practicing/main.py
@@ -0,0 +1,22 @@
+import datetime as dt
+import smtplib
+import random
+
+now = dt.datetime.now()
+my_email = "pethersonreis@gmail.com"
+password = "mpcf zyug mygv zefj"
+
+if now.weekday() == 6:
+    with open("quotes.txt", mode="r", encoding="utf-8") as file:
+        data = file.read()
+        quotes = data.split('\n')
+        day_quote = random.choice(quotes)
+        print(day_quote)
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="mayson.reiz@gmail.com",
+            msg=f"Subject:Monday Motivation\n\n{day_quote}".encode("utf-8"))
