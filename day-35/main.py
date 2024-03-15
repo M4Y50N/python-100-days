83f035d6ce13aea0992c6e1c8c2f75f51adee38a
pethe <pethersonreis@gmail.com>
2024-03-15 16:08:16 -0300

    feat: call weather track api

--- day-35/main.py
+++ day-35/main.py
@@ -1,7 +1,23 @@
+from dotenv import load_dotenv
+import os
 import requests
 
+load_dotenv()
 
+END_PONT = "https://api.openweathermap.org/data/2.5/forecast"
+API_KEY = os.getenv("API_KEY")
+MY_LON = float(os.getenv("MY_LON"))
+MY_LAT = float(os.getenv("MY_LAT"))
 
+params = {
+    "lat": MY_LAT,
+    "lon": MY_LON,
+    "count": 4,
+    "appid": API_KEY,
+}
+
+response = requests.get(url=END_PONT, params=params)
+print(response.json())
 
 
 
