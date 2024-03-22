436698c4e3f0d3f7d6502d7ee14eea72afa5ac7c
pethe <pethersonreis@gmail.com>
2024-03-22 00:26:57 -0300

    feat: finish pixela project

--- day-37/main.py
+++ day-37/main.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from datetime import datetime
 import requests
 import os
 
@@ -17,19 +18,39 @@ USERNAME = "seuotacilio"
 
 # response = requests.post(url=pixela_endpoint, json=user_params)
 
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
-graph_config = {
-    "id": "graph1",
-    "name": "Cycling Graph",
-    "unit": "Km",
-    "type": "float",
-    "color": "ajisai"
-}
-
+# graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+# graph_config = {
+#     "id": "graph1",
+#     "name": "Cycling Graph",
+#     "unit": "Km",
+#     "type": "float",
+#     "color": "ajisai"
+# }
+#
 headers = {
     "X-USER-TOKEN": PIXELA_SECRET
 }
+#
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{"graph1"}"
+today = datetime.now()
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "13.3"
+}
 
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
 print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{"graph1"}/{today.strftime("%Y%m%d")}"
+new_pixel_data = {
+    "quantity": "10"
+}
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{"graph1"}/{today.strftime("%Y%m%d")}"
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
