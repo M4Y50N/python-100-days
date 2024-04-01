dee03a1f1b36d2450ecd83ebba9c24a17f2658f9
pethe <pethersonreis@gmail.com>
2024-03-31 22:34:16 -0300

    feat: user flight api and get the iataCode

--- day-39/main.py
+++ day-39/main.py
@@ -15,13 +15,31 @@ sheety_header = {
 sheety_request = requests.get(sheet_endpoint, headers=sheety_header)
 data = sheety_request.json()
 
-for price in data["prices"]:
-    put_request = requests.put(sheet_endpoint + f"/{price["id"]}",
-                               json={
-                                   "price":
-                                       {"iataCode": "TESTING"}
-                               }, headers=sheety_header,
-                               )
+# for price in data["prices"]:
+#     put_request = requests.put(sheet_endpoint + f"/{price["id"]}",
+#                                json={
+#                                    "price":
+#                                        {"iataCode": ""}
+#                                }, headers=sheety_header,
+#                                )
     # print(put_request.json())
 
 # pprint(data)
+
+tequila_endpoint = os.getenv("TEQUILA_ENDPOINT")
+tequila_affilid = os.getenv("TEQUILA_AFFILID")
+tequila_apikey = os.getenv("TEQUILA_APIKEY")
+
+header = {
+    "apikey": tequila_apikey,
+}
+query = {
+    "term": "paris",
+    "location_types": "city"
+}
+response = requests.get(url=tequila_endpoint, headers=header, params=query)
+
+print(response.json())
+
+
+
