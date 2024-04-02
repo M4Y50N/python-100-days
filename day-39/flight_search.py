084fc8354096fbc94155677b6edb20f2bb9fc8b9
pethe <pethersonreis@gmail.com>
2024-04-01 23:24:40 -0300

    feat: get GRU -> AJU flights

--- day-39/flight_search.py
+++ day-39/flight_search.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from dotenv import load_dotenv
 import requests
 import os
@@ -11,16 +13,33 @@ TEQUILA_APIKEY = os.getenv("TEQUILA_APIKEY")
 
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
-    def get_iata_code(self, city_name):
-        header = {
+    def __init__(self):
+        self.header = {
             "apikey": TEQUILA_APIKEY,
         }
 
+    def get_iata_code(self, city_name):
         query = {
             "term": city_name,
             "location_types": "city"
         }
 
-        response = requests.get(url=f"{TEQUILA_ENDPOINT}/locations/query", headers=header, params=query)
+        response = requests.get(url=f"{TEQUILA_ENDPOINT}/locations/query", headers=self.header, params=query)
 
         return response.json()["locations"][0]["code"]
+
+    def get_flight(self):
+        query = {
+            "fly_from": "SAO",
+            "fly_to": "AJU",
+            "date_from": "19/04/2024",
+            "date_to": "21/04/2024",
+            "max_stopovers": 0,
+            "curr": "BRL"
+        }
+
+        response = requests.get(url=f"{TEQUILA_ENDPOINT}/v2/search", headers=self.header, params=query)
+
+        for flight in response.json()["data"]:
+            print(flight["price"])
+            print(flight["deep_link"])
--- day-39/main.py
+++ day-39/main.py
@@ -6,13 +6,16 @@ from data_manager import DataManager
 data_manager = DataManager()
 data_manager.get_flights()
 
-# Update IATA codes
-for data in data_manager.sheet_data:
-    # Check if IATA code is empty
-    if data["iataCode"] == "":
-        fight_search = FlightSearch()
-        code = fight_search.get_iata_code(data["city"])
-        data["iataCode"] = code
+fs = FlightSearch()
+fs.get_flight()
 
-        data_manager.update_iata(city_id=data["id"], iata_code=data["iataCode"])
+# # Update IATA codes
+# for data in data_manager.sheet_data:
+#     # Check if IATA code is empty
+#     if data["iataCode"] == "":
+#         fight_search = FlightSearch()
+#         code = fight_search.get_iata_code(data["city"])
+#         data["iataCode"] = code
+#
+#         data_manager.update_iata(city_id=data["id"], iata_code=data["iataCode"])
 
