862270804a7468f567825c36eb7febb95fc47474
peth <pethersonreis@gmail.com>
2024-04-03 23:47:03 -0300

    feat: check if price is the lowest

--- day-39/main.py
+++ day-39/main.py
@@ -1,7 +1,7 @@
 from flight_search import FlightSearch
 from data_manager import DataManager
-
 from datetime import datetime, timedelta
+import math
 
 ORIGIN_CITY = "SAO"
 
@@ -9,21 +9,28 @@ data_manager = DataManager()
 data_manager.get_flights()
 
 # Update IATA codes
-is_empty = False
+is_updatable = False
 for index, data in enumerate(data_manager.sheet_data):
     # Check if IATA code is empty
     if isinstance(data["IATA Code"], float):
-        is_empty = True
+        is_updatable = True
         fight_search = FlightSearch()
         code = fight_search.get_iata_code(data["City"])
         data["IATA Code"] = code
 
-if is_empty:
-    data_manager.update_iata()
-
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
 
-fs = FlightSearch()
 for destination in data_manager.sheet_data:
-    fs.get_flight(ORIGIN_CITY, destination["IATA Code"], tomorrow, six_month_from_today)
+    fs = FlightSearch()
+    flight = fs.get_flight(ORIGIN_CITY, destination["IATA Code"], tomorrow, six_month_from_today)
+
+    if flight:
+        if flight.price < destination["Lowest Price"] or math.isnan(destination["Lowest Price"]):
+            is_updatable = True
+            destination["Lowest Price"] = flight.price
+
+if is_updatable:
+    data_manager.update_iata()
+
+
