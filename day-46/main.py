f045236ca323d03fe0463f7b667f200554e9072f
peth <pethersonreis@gmail.com>
2024-04-07 22:44:22 -0300

    feat: create playlist and add songs to it

--- day-46/main.py
+++ day-46/main.py
@@ -10,9 +10,9 @@ import os
 
 load_dotenv()
 
-# data_input = input("Which year you want to travel to? Type the date in this format YYYY-MM-DD:")
-data_input = "2023-03-04"
-response = requests.get(f"https://www.billboard.com/charts/hot-100/{data_input}/")
+date_input = input("Which year you want to travel to? Type the date in this format YYYY-MM-DD:")
+year = date_input.split("-")[0]
+response = requests.get(f"https://www.billboard.com/charts/hot-100/{date_input}/")
 
 soup = BeautifulSoup(response.text, "lxml")
 
@@ -25,7 +25,23 @@ CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
 REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
 
-client_credentials = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI)
+client_credentials = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI,
+                                  scope="playlist-modify-private")
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials)
 
-print(spotify.current_user()["id"])
+user_id = spotify.current_user()["id"]
+
+# Get songs url
+songs_url = []
+for song in songs:
+    result = spotify.search(q=f"track:{song} year:{year}", type="track")
+    try:
+        songs_url.append(result["tracks"]["items"][0]["uri"])
+    except IndexError:
+        print(f"'{song}' doesn't exist in Spotify. Skipped.")
+
+playlist = spotify.user_playlist_create(user=user_id, name=f"{date_input} Billboard 100", public=False)
+# print(playlist["id"])
+
+spotify.playlist_add_items(playlist["id"], songs_url)
+
