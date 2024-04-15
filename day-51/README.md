46687dffb9789cb9b1c604d4024ac0188a5c627f
peth <pethersonreis@gmail.com>
2024-04-15 19:22:22 -0300

    feat: finish day-51 project

---
+++ day-51/README.md
@@ -0,0 +1,8 @@
+# Post My Connection Speed On Twitter
+
+### Day 51, this project is able to get your connection speed and post it on Twitter.
+### You need to create a .env file and fill with your twitter email and password 
+
+### Used Libraries: Selenium
+
+#### Note: Doesn't work at the moment because of robot verification
---
+++ day-51/main.py
@@ -0,0 +1,46 @@
+import os
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://www.speedtest.net/")
+
+time.sleep(3)
+
+driver.find_element(By.XPATH, value='//*[@id="onetrust-accept-btn-handler"]').click()
+driver.find_element(By.XPATH, value='//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]').click()
+
+time.sleep(45)
+
+download = driver.find_element(By.XPATH, value='/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
+upload = driver.find_element(By.XPATH, value='/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+
+# Loging in Twitter
+driver.get("https://twitter.com/login")
+
+email = os.getenv("EMAIL")
+password = os.getenv("PASSWORD")
+message = f"Minha taxa de Download e Upload são {download}/{upload}"
+
+# driver.find_element(By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[5]/a').click()
+time.sleep(5)
+driver.find_element(By.XPATH, value='//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input').send_keys(email, Keys.ENTER)
+time.sleep(5)
+driver.find_element(By.XPATH, value='//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input').send_keys(password, Keys.ENTER)
+
+# Post on twitter
+driver.find_element(By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div/div[2]/div/div/div/div').send_keys(message)
+driver.find_element(By.XPATH, value='/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div/div/div[3]/div/span').click()
+
+
